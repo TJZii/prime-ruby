040e7d8136675ecc46bb7b1ef8c0617152fa4e3b
TJZii <tjhaweszii@gmail.com>
2020-11-22 09:54:48 -0800

    commit tree 7673c0bb0e71ccee3e0fe350d71774fbf3550df0

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
 # Add  code here!
+
+def prime?(arg)
+    if(arg <= 1)
+        return false
+    end
+    z = 2
+    while z < arg
+        if(arg % z) == 0
+            return false
+        end
+        z += 1
+    end
+    return true
+end
